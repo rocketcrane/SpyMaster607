e1bb8044ed152ce029a93598067031bf860ef140
Lingxiu Zhang <47123365+rocketcrane@users.noreply.github.com>
2024-02-16 14:03:00 -0800

    test

--- b.py
+++ b.py
@@ -63,13 +63,18 @@ def list_input_device(p):
 		# print(deviceInfo)
 
 def remap_range(value, left_min, left_max, right_min, right_max):
+	# log addition
+	log_left_min = math.log(left_min + 1e-9)
+	log_left_max = math.log(left_max)
+	log_value = math.log(value)
+	
 	# this remaps a value from original (left) range to new (right) range
 	# Figure out how 'wide' each range is
-	left_span = left_max - left_min
+	left_span = log_left_max - log_left_min
 	right_span = right_max - right_min
 	
 	# Convert the left range into a 0-1 range (int)
-	valueScaled = float(value - left_min) / float(left_span)
+	valueScaled = float(log_value - log_left_min) / float(left_span)
 	
 	# Convert the 0-1 range into a value in the right range.
 	return double(right_min + (valueScaled * right_span))
