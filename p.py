304563d629092a133f85b8f13f844ff417f8e57a
Lingxiu Zhang <47123365+rocketcrane@users.noreply.github.com>
2024-02-17 16:57:04 -0800

    pot

--- p.py
+++ p.py
@@ -33,10 +33,11 @@ def remap_range(value, in_min, in_max, out_min, out_max):
 
     # Convert the left range into a 0-1 range
     valueScaled = float(value - in_min) / float(in_span)
+    print("this should be between 0 and 1: ", valueScaled)
     
     # Logarithmically map the value
     # first, scale the value to a 1-11 range
-    valueScaled = float(1 + (valueScaled * 10))
+    valueScaled = float(1 + (valueScaled * 9))
     # then, map it to log base 10
     valueScaled = math.log(valueScaled)
     
@@ -57,7 +58,7 @@ while True:
     # convert 16bit adc0 (0-65535) trim pot read into 0-100 volume level
     set_volume = remap_range(trim_pot, 0, 65535, 0, 100)
     
-    print("trim pot is ", trim_pot, " remapped to ", set_volume)
+    # print("trim pot is ", trim_pot, " remapped to ", set_volume)
 
     # save the potentiometer reading for the next loop
     last_read = trim_pot
